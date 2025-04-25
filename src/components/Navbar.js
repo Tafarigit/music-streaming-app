202cb5854089fac50aac371274626b650b68c154
Tafari Excell <excell.tafari@gmail.com>
2025-04-25 13:47:34 -0400

    implemented codeblock that esptablishes the link to several files in the pages folder including,songs, users, login and Home

---
+++ src/components/Navbar.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+    return (
+        <nav>
+            <Link to="/">Home</Link>{" | "}
+            <Link to="/songs">Songs</Link>{" | "}
+            <Link to="/users">Users</Link>{" | "}
+            <Link to="/login">Login</Link>
+        </nav>
+    );
+};
+
+
+
+
+
+export default Navbar;
