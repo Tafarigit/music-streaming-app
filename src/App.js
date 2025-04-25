fc5193d98b824de41f950cd2f314db244e87352f
Tafari Excell <excell.tafari@gmail.com>
2025-04-25 13:41:17 -0400

    Imported react, react-router-dom all the files from the pages folder.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
+
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Navbar from "./comnponents/Navbar";
+import Home from "./pages/Home";
+import Songs from "./pages/Songs";
+import SongDetails from "./pages/SongDetails";
+import Users from "./pages/Users";
+import Login from "./pages/Login";
+
+
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/songs" element={<Songs />} />
+          <Route path="/songs:id" element={<SongDetails />} />
+          <Route path="/users" element={<Users />} />
+          <Route path="/Login" element={<Login />} />
+        </Routes>
+    
+    </Router>
+  
   );
 }
 
